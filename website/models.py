b969a5bc8fb875891da74f3ea3b6f4bb4fdfe08a
Guilherme Brito <pinheiro.g88@gmail.com>
2019-09-25 11:06:13 -0300

    Diversas funções realizadas para o projeto funcionar

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Coach(models.Model):
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    frase = models.TextField()
+    inspirador = models.CharField(max_length=255, null=True) ## null=True é campo opcional
+
+    criado_em =  models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self): 
+        return self.nome ### Retornar no admin o nome do usuário cadastrou.
--- website/views.py
+++ website/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from website.models import *
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        coach = Coach()
+        coach.nome = request.POST['nome']
+        coach.frase = request.POST['frase']
+        coach.inspirador = request.POST['inspirador']
+        coach.save()
+
+        args = {
+            'msg': 'Até quem fim meus programas estão dando certo Uhuuuuuuuuuuuuuuu'
+        }
+        return render(request, 'index.html', args)
+    
     return render(request, 'index.html')
 
